095006f84240563883ba1be9a4503d1fdf97d35b
n24chris05 <ChrisMacBookPro@Chriss-MacBook-Pro.local>
2014-05-24 19:22:48 +0800

    update

---
+++ HomePwner10/HomePwner/BNRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRDetailViewController.h
+//  HomePwner
+//
+//  Created by Chris Arquelada on 5/23/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRDetailViewController : UIViewController
+
+@end
