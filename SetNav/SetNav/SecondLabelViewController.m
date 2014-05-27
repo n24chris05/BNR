c97995258f1ce6e9fb2d7c7fdb07f9155c0ad401
n24chris05 <ChrisiMac@Chriss-iMac-5.local>
2014-05-28 00:21:06 +0800

    2

---
+++ SetNav/SetNav/SecondLabelViewController.m
@@ -0,0 +1,17 @@
+//
+//  SecondLabelViewController.m
+//  SetNav
+//
+//  Created by Chris Arquelada on 5/27/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "SecondLabelViewController.h"
+
+@interface SecondLabelViewController ()
+
+@end
+
+@implementation SecondLabelViewController
+
+@end
