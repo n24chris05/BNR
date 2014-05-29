b539043c23f94db08db2394b6d4434757fa06a68
n24chris05 <ChrisiMac@Chriss-iMac-5.local>
2014-05-29 16:19:43 +0800

    123

--- SetNav/SetNav/DisclosureButtonViewController.m
+++ SetNav/SetNav/DisclosureButtonViewController.m
@@ -38,22 +38,7 @@ static NSString *CellIdentifier =@"Cell";
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CellIdentifier];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 #pragma mark -Table View data source
 -(NSInteger )tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
